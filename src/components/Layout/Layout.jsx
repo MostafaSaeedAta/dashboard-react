74e1595fa9e40fb3d452880c468f01bda60e76e6
unknown <you@example.com>
2023-03-08 15:36:53 +0200

    56

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,20 @@
 import moment from 'moment/moment'
 import css from './Layout.module.css'
 import { IoIosSearch } from "react-icons/io";
+import Sidebar from '../Sidebar/Sidebar';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 
 const Layout = () => {
+
+  const { pathname } = useLocation() ;  
   return (
     <div className={css.container}>
 
-      <div className=''>sidebar</div>
+       <Sidebar/>  
+
+       { /* make dashboard as first route */ }
+       { pathname === '/' && <Navigate to='/dashboard' /> }
 
       <div className={css.dashboard}>
         <div className={css.topBaseGradients}>
@@ -18,11 +25,26 @@ const Layout = () => {
 
         <div className={css.header}>
           <span> { moment().format('dddd, Do  MM YYYY') } </span>
+
           <div className={css.searchBar}>
             <IoIosSearch size={20} />
             <input type='text' placeholder='search'/>
           </div>
+
+          <div className={css.profile}>
+            <img src='profile.png' alt='person image' />
+            <div className={css.details}>
+              <span> mostafa saeed </span>
+              <span> mostafa123233@gmail.com </span>
+            </div>
+          </div>
+
         </div>
+
+        <div className={css.content}>
+            <Outlet />
+        </div>
+
       </div>
 
     </div>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,48 @@
- import './Dashboard.module.css'
+ import { cardsData } from '../../data'
+import css from './Dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <div>Dashdboard</div>
+    <div className={css.container}>
+    
+      {/* left side */}
+      <div className={css.dashboard}>
+        
+        <div className={ `${css.dashboardHead} theme-container ` }>
+          <div className={css.head}>
+            <span> dashboard </span>
+
+            <div className={css.durationButton}>
+              <select>
+                <option value=''> 1 weak</option>
+                <option value=''> 1 month</option>
+                <option value=''> 1 year</option>
+              </select>
+            </div>
+          </div>
+
+          <div className={css.cards}>
+            {
+              cardsData.map((card, index)=>(
+                <div className={css.card}>
+                <div className={css.cardHead}>
+                  <span>{card.title}</span>
+                  <span>{card.change}</span>
+                </div>
+               
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        
+      </div>
+
+      <div className={css.orders}>
+        orders
+      </div>
+
+    </div>
   )
 }
 
